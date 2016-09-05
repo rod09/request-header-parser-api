d1f335eb371636624465c1acd7d129cac62e65c0
Rodney <rodney.agro@gmail.com>
2016-09-05 01:19:00 +0000

    challenge completed

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require("express");
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send(req.ip);
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var language = req.acceptsLanguages()[0];
+    var os = req.headers['user-agent'];
+
+    res.send(JSON.stringify({
+        ipaddress: ip,
+        language: language,
+        software: os
+    }));
 });
 
 app.listen(8080, () => {
